5be391430e0c7d0b3f4f0d5ca81edc320b5204b2
Michael de Jong <michaelj@minicom.nl>
2014-09-02 10:04:34 +0200

    Added a dummy JUnit test.

---
+++ src/test/java/nl/tudelft/ti2206/game/DummyTest.java
@@ -0,0 +1,15 @@
+package nl.tudelft.ti2206.game;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void dummyTest() {
+		assertThat(true, is(true));
+	}
+	
+}
