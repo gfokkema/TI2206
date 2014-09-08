87c848c5af15c5fb199185180cb451ea7de3ea7e
Balletie <skip_meesie@hotmail.com>
2014-09-08 22:43:45 +0200

    Bubble: Add utility functions: get/setNeighbor, Orientation.getOpposite

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/Bubble.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/Bubble.java
@@ -1,5 +1,7 @@
 package nl.tudelft.ti2206.bubbleshooter;
 
+import java.util.Optional;
+
 import com.badlogic.gdx.math.Circle;
 
 public class Bubble {
@@ -9,8 +11,19 @@ public class Bubble {
 		SOUTH_EAST,
 		SOUTH_WEST,
 		WEST,
-		NORTH_WEST
+		NORTH_WEST;
+
+		/**
+		 * Returns the opposite orientation,
+		 * for example SOUTH_EAST.getOpposite()
+		 * will return NORTH_EAST.
+		 * @return the opposite orientation
+		 */
+		public Orientation getOpposite() {
+			return orientations[this.ordinal() + 3];
+		}
 	}
+	private static final Orientation[] orientations = Orientation.values();
 
 	public enum Color {
 		RED,
@@ -23,11 +36,16 @@ public class Bubble {
 	private Color color;
 	private Bubble[] neighbors;
 	private Circle bounds;
-	private boolean isRoot;
-	private boolean marked;
 
 	public Bubble() {
 		this.neighbors = new Bubble[6];
 	}
 
+	public Optional<Bubble> getNeighbor(Orientation dir) {
+		return Optional.ofNullable(neighbors[dir.ordinal()]);
+	}
+
+	public void setNeighbor(Bubble b, Orientation dir) {
+		neighbors[dir.ordinal()] = b;
+	}
 }
