a69ecb6d8b5e2c1244987f34b7d45415c848772c
Kartoredjo <N.S.Kartoredjo@student.tudelft.nl>
2014-09-08 20:52:11 +0200

    orginize classes
    add map artifacts
    rename cannon to pointer
    create cannon

---
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/core/artifacts/Cannon.java
@@ -0,0 +1,42 @@
+package nl.tudelft.ti2206.bubbleshooter.core;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Create a cannon to shoot bubbles
+ * @author Nando
+ *
+ */
+public class Cannon {
+
+	Pointer pointer;
+	Vector2 cursor;
+	Texture image;
+	
+	public Cannon(Vector2 vector) {
+		cursor = vector;
+		pointer = new Pointer(vector);
+	}
+	
+	public void SetAngle() {
+		
+	}
+	
+	public void setPosition() {
+		
+	}
+	
+	public Pointer getPointer() {
+		return pointer;
+	}
+	
+	public void shoot() {
+		
+	}
+	
+	public void spawnBubble() {
+		
+	}
+	
+}
