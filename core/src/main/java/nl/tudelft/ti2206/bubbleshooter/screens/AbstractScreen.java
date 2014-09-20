aa621d829708ee9df10f2858a055ce0537231cfd
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-09-20 22:31:49 +0200

    Added background

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/screens/AbstractScreen.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/screens/AbstractScreen.java
@@ -1,13 +1,16 @@
 package nl.tudelft.ti2206.bubbleshooter.screens;
 
 import nl.tudelft.ti2206.bubbleshooter.Launch;
+import nl.tudelft.ti2206.bubbleshooter.engine.Assets.TextureID;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class AbstractScreen extends ScreenAdapter {
@@ -24,6 +27,9 @@ public class AbstractScreen extends ScreenAdapter {
 		this.stage = new Stage(new ScreenViewport());
 		this.table = new Table();
 		
+		TextureRegion region = new TextureRegion(game.assets.get(TextureID.BACKGROUND));
+		TextureRegionDrawable drawable = new TextureRegionDrawable(region);
+		table.background(drawable);
 		table.setFillParent(true);
 	}
 	
