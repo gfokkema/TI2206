3afad5630b243ca39368ba6f44bd56e9967013c8
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-09-21 01:09:46 +0200

    Options screen has now been beautified as well

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/screens/OptionsScreen.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/screens/OptionsScreen.java
@@ -1,142 +1,98 @@
 package nl.tudelft.ti2206.bubbleshooter.screens;
 
-import java.util.ArrayList;
-
 import nl.tudelft.ti2206.bubbleshooter.Launch;
-import nl.tudelft.ti2206.bubbleshooter.engine.Assets.MusicID;
 import nl.tudelft.ti2206.bubbleshooter.engine.Assets.SoundID;
-import nl.tudelft.ti2206.bubbleshooter.utils.Button;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
  * The options screen, where the player may change volume settings.
  * @author group-15
  *
  */
-public class OptionsScreen extends ScreenAdapter {
+public class OptionsScreen extends AbstractScreen {
 	/**
 	 * Variable initialization.
 	 * Set the screen title.
 	 */
 	public static final String title = "Options";
 	private final float volumeStep = 0.1f;
-
-	Launch game;
-	ArrayList<Button> buttons;
 	
 	/**
 	 * Sets up the buttons to be displayed.
 	 * @param game the current game session
 	 */
 	public OptionsScreen(Launch game) {
-		this.game = game;
-		this.buttons = new ArrayList<Button>();
-
-		// Button that turns the BGM volume up!
-		Button BGMvolup = new Button(
-				new Rectangle(Gdx.graphics.getWidth() / 2 - 125, Gdx.graphics.getHeight() / 2 + 50, 100, 50),
-				new Color(0xFFFF00FF),
-				game.font,
-				"Volume Up!",
-				() -> { game.engine.setBGMVolume(game.engine.getBGMVolume() + volumeStep); game.engine.play(SoundID.BUTTON); }
-		);
+		super(game);
 		
-		// Button that turns the BGM volume down!
-		Button BGMvoldown = new Button(
-				new Rectangle(Gdx.graphics.getWidth() / 2 + 50, Gdx.graphics.getHeight() / 2 + 50, 100, 50),
-				new Color(0xFFFF00FF),
-				game.font,
-				"Volume Down!",
-				() -> { game.engine.setBGMVolume(game.engine.getBGMVolume() - volumeStep); game.engine.play(SoundID.BUTTON); }
-		);
+		LabelStyle labelstyle = new LabelStyle(game.font, Color.WHITE);
+		TextButtonStyle style = new TextButtonStyle();
+		style.font = game.font;
 		
-		// Button that turns the SFX volume up!
-		Button SFXvolup = new Button(
-				new Rectangle(Gdx.graphics.getWidth() / 2 - 125, Gdx.graphics.getHeight() / 2 - 25, 100, 50),
-				new Color(0xFFFF00FF),
-				game.font,
-				"SFX Up!",
-				() -> { game.engine.setSFXVolume(game.engine.getSFXVolume() + volumeStep); game.engine.play(SoundID.BUTTON); }
-		);
+		Label vollabel = new Label("Change Volume", labelstyle);
+		TextButton volup = new TextButton("+", style);
+		TextButton voldown = new TextButton("-", style);
+		Label sfxlabel = new Label("Change SFX", labelstyle);
+		TextButton sfxup = new TextButton("+", style);
+		TextButton sfxdown = new TextButton("-", style);
 		
-		// Button that turns the SFX volume down!
-		Button SFXvoldown = new Button(
-				new Rectangle(Gdx.graphics.getWidth() / 2 + 50, Gdx.graphics.getHeight() / 2 - 25, 100, 50),
-				new Color(0xFFFF00FF),
-				game.font,
-				"SFX Down!",
-				() -> { game.engine.setSFXVolume(game.engine.getSFXVolume() - volumeStep); game.engine.play(SoundID.BUTTON); }
-		);
+		volup.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				game.engine.play(SoundID.BUTTON);
+				game.engine.setBGMVolume(game.engine.getBGMVolume() + volumeStep);
+			}
+		});
+		voldown.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				game.engine.play(SoundID.BUTTON);
+				game.engine.setBGMVolume(game.engine.getBGMVolume() - volumeStep);
+			}
+		});
+		sfxup.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				game.engine.play(SoundID.BUTTON);
+				game.engine.setSFXVolume(game.engine.getSFXVolume() + volumeStep);
+			}
+		});
+		sfxdown.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				game.engine.play(SoundID.BUTTON);
+				game.engine.setSFXVolume(game.engine.getSFXVolume() - volumeStep);
+			}
+		});
 		
-		// Button that sends the player back to the main menu.
-		Button back = new Button(
-				new Rectangle(Gdx.graphics.getWidth() / 2 - 100, Gdx.graphics.getHeight() / 2 - 100, 200, 50),
-				new Color(0xFFFF00FF),
-				game.font,
-				"Back",
-				() -> { game.setScreen(game.mms); game.engine.play(SoundID.BUTTON); }
-		);
+		HorizontalGroup volgroup = new HorizontalGroup();
+		volgroup.space(20);
+		volgroup.addActor(volup);
+		volgroup.addActor(voldown);
 		
-		//Add buttons, each with their own callback.
-		buttons.add(BGMvolup);
-		buttons.add(BGMvoldown);
-		buttons.add(SFXvolup);
-		buttons.add(SFXvoldown);
-		buttons.add(back);
+		HorizontalGroup sfxgroup = new HorizontalGroup();
+		sfxgroup.space(20);
+		sfxgroup.addActor(sfxup);
+		sfxgroup.addActor(sfxdown);
+		
+		table.add(vollabel).expandX().center().row();
+		table.add(volgroup).expandX().center().row();
+		table.add(sfxlabel).expandX().center().row();
+		table.add(sfxgroup).expandX().center().row();
 	}
 	
-	/**
-	 * Render the buttons
-	 */
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-		update();
-		game.batch.begin();
-
-		game.font.setColor(new Color(0xFFFF00FF));
-		game.font.draw(game.batch, title, Gdx.graphics.getWidth() / 2 + 50, Gdx.graphics.getHeight() / 2 + 125);
-
-		buttons.forEach((Button b) -> b.draw(game.batch));
-		game.batch.end();
-	}
-
-	/**
-	 * Update during each frame. Called by {@link #render(float)}
-	 * Checks if buttons were hit and reacts to it.
-	 */
-	private void update() {
-        boolean left_down = Gdx.input.justTouched();
-		if (left_down) {
-			int x = Gdx.input.getX();
-			int y = Gdx.graphics.getHeight() - Gdx.input.getY();
-			buttons.stream()
-				.filter((Button b) -> b.hit(x,y))
-				.findFirst()
-				.ifPresent(Button::apply);
-		}
-	}
-	
-	/**
-	 * Play some background music.
-	 * This music will be played in the main menu screen and options screen
-	 */
-	@Override
-	public void show() {
-		game.engine.play(MusicID.MENU);
-	}
-	
-	/**
-	 * Hide is being called when the main menu screen is not the current screen.
-	 */
-	@Override
-	public void hide() {
-		game.engine.pause();
+		super.render(delta);
+		if (Gdx.input.isKeyPressed(Keys.ESCAPE)) game.setScreen(game.mms);
 	}
 }
