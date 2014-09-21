6979e3a915478cb3d03295af5cad5e46f9a71ad5
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-09-21 22:09:50 +0200

    Documented AbstractProcessor

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/input/AbstractProcessor.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/input/AbstractProcessor.java
@@ -6,28 +6,71 @@ import nl.tudelft.ti2206.bubbleshooter.mode.BSMode;
 
 import com.badlogic.gdx.InputAdapter;
 
+/**
+ * This class processes input from a game.
+ * It will listen for input from the controls and call actions based on that.
+ * A singleplayerprocessor or hotseatedprocessor should be derived from this class.
+ */
 public abstract class AbstractProcessor extends InputAdapter {
 	protected BSMode mode;
 
+	/**
+	 * Create a {@link AbstractProcessor} which will control the given {@link BSMode}.
+	 * @param BSMode	the {@link BSMode} to control
+	 */
 	public AbstractProcessor(BSMode BSMode) {
 		this.mode = BSMode;
 	}
 
+	/**
+	 * This function is called when a key is pressed down.
+	 * @param keyCode	integer indicating which key was pressed
+	 * @return	boolean indicating whether the event has been handled
+	 */
 	@Override
 	public boolean keyDown(int keyCode) {
 		if (!keyDownBindings.containsKey(keyCode)) return false;
 		return keyDownBindings.get(keyCode).apply(this);
 	}
 
+	/**
+	 * This function is called when a key is let go.
+	 * @param keyCode	integer indicating which key was pressed
+	 * @return	boolean indicating whether the event has been handled
+	 */
 	@Override
 	public boolean keyUp(int keyCode) {
 		if (!keyUpBindings.containsKey(keyCode)) return false;
 		return keyUpBindings.get(keyCode).apply(this);
 	}
 
+	/**
+	 * This function will start rotating the cannon in the {@link BSMode} to the left.
+	 * @return	boolean indicating whether the cannon has started rotating
+	 */
 	public abstract boolean cannonLeft();
+	
+	/**
+	 * This function will start rotating the cannon in the {@link BSMode} to the right.
+	 * @return	boolean indicating whether the cannon has started rotating
+	 */
 	public abstract boolean cannonRight();
+	
+	/**
+	 * This function is called to shoot the cannon in the {@link BSMode} game.
+	 * @return	boolean indicating whether the cannon could be fired
+	 */
 	public abstract boolean cannonShoot();
+	
+	/**
+	 * This function will stop rotating the cannon in the {@link BSMode} to the left.
+	 * @return	boolean indicating whether the cannon has stopped rotating
+	 */
 	public abstract boolean cannonStopLeft();
+	
+	/**
+	 * This function will stop rotating the cannon in the {@link BSMode} to the right.
+	 * @return	boolean indicating whether the cannon has stopped rotating
+	 */
 	public abstract boolean cannonStopRight();
 }
--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/input/SinglePlayerProcessor.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/input/SinglePlayerProcessor.java
@@ -4,8 +4,7 @@ import nl.tudelft.ti2206.bubbleshooter.mode.BSMode;
 
 /**
  * This class processes input from a single player game.
- * It will listen for input from the controls and call actions based on that. 
- *
+ * It will listen for input from the controls and call actions based on that.
  */
 public class SinglePlayerProcessor extends AbstractProcessor {
 	/**
@@ -20,6 +19,7 @@ public class SinglePlayerProcessor extends AbstractProcessor {
 	 * This function is called to shoot the cannon in the {@link BSMode} game.
 	 * @return	boolean indicating whether the cannon could be fired
 	 */
+	@Override
 	public boolean cannonShoot() {
 		if (mode.getProjectile() == null) {
 			mode.setProjectile(mode.getCannon().shoot());
@@ -32,6 +32,7 @@ public class SinglePlayerProcessor extends AbstractProcessor {
 	 * This function will start rotating the cannon in the {@link BSMode} to the left.
 	 * @return	boolean indicating whether the cannon has started rotating
 	 */
+	@Override
 	public boolean cannonLeft() {
 		mode.cannonLeft(true);
 		return true;
@@ -41,6 +42,7 @@ public class SinglePlayerProcessor extends AbstractProcessor {
 	 * This function will start rotating the cannon in the {@link BSMode} to the right.
 	 * @return	boolean indicating whether the cannon has started rotating
 	 */
+	@Override
 	public boolean cannonRight() {
 		mode.cannonRight(true);
 		return true;
@@ -50,6 +52,7 @@ public class SinglePlayerProcessor extends AbstractProcessor {
 	 * This function will stop rotating the cannon in the {@link BSMode} to the left.
 	 * @return	boolean indicating whether the cannon has stopped rotating
 	 */
+	@Override
 	public boolean cannonStopLeft() {
 		mode.cannonLeft(false);
 		return true;
@@ -59,6 +62,7 @@ public class SinglePlayerProcessor extends AbstractProcessor {
 	 * This function will stop rotating the cannon in the {@link BSMode} to the right.
 	 * @return	boolean indicating whether the cannon has stopped rotating
 	 */
+	@Override
 	public boolean cannonStopRight() {
 		mode.cannonRight(false);
 		return true;
