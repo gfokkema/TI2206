6c4f5296af351d5990bc2211a1305894600d5a97
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-09-21 20:45:06 +0200

    JavaDOC for BSDrawable

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/core/Background.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/core/Background.java
@@ -3,6 +3,9 @@ package nl.tudelft.ti2206.bubbleshooter.core;
 import nl.tudelft.ti2206.bubbleshooter.engine.Assets.TextureID;
 import nl.tudelft.ti2206.bubbleshooter.engine.BSDrawable;;
 
+/**
+ * This class holds the properties of the background to use for the game.
+ */
 public class Background extends BSDrawable {
 	@Override
 	public TextureID getTexture() {
