07fe4eeac14ed6a370a3f1d93ee72ab5a78f5315
Balletie <skip_meesie@hotmail.com>
2014-09-21 15:34:08 +0200

    Input: Use AbstractProcessor for future different processors, initialize keybindings in BubbleShooter

---
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/mode/AbstractProcessor.java
@@ -0,0 +1,31 @@
+package nl.tudelft.ti2206.bubbleshooter.mode;
+
+import static nl.tudelft.ti2206.bubbleshooter.BubbleShooter.keyDownBindings;
+import static nl.tudelft.ti2206.bubbleshooter.BubbleShooter.keyUpBindings;
+
+import com.badlogic.gdx.InputAdapter;
+
+public abstract class AbstractProcessor extends InputAdapter {
+	protected BSMode mode;
+
+	public AbstractProcessor(BSMode BSMode) {
+		this.mode = BSMode;
+	}
+
+	@Override
+	public boolean keyDown(int keyCode) {
+		if (!keyDownBindings.containsKey(keyCode)) return false;
+		return keyDownBindings.get(keyCode).apply(this);
+	}
+
+	@Override
+	public boolean keyUp(int keyCode) {
+		if (!keyUpBindings.containsKey(keyCode)) return false;
+		return keyUpBindings.get(keyCode).apply(this);
+	}
+
+	public abstract boolean cannonLeft();
+	public abstract boolean cannonRight();
+	public abstract boolean cannonShoot();
+	public abstract boolean cannonStopMoving();
+}
--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/mode/SinglePlayerProcessor.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/mode/SinglePlayerProcessor.java
@@ -1,34 +1,9 @@
 package nl.tudelft.ti2206.bubbleshooter.mode;
 
-import static nl.tudelft.ti2206.bubbleshooter.BubbleShooter.keyDownBindings;
-import static nl.tudelft.ti2206.bubbleshooter.BubbleShooter.keyUpBindings;
-
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputAdapter;
-
-public class SinglePlayerProcessor extends InputAdapter {
-	private BSMode mode;
+public class SinglePlayerProcessor extends AbstractProcessor {
 
 	public SinglePlayerProcessor(BSMode BSMode) {
-		this.mode = BSMode;
-		keyDownBindings.put(Keys.LEFT, SinglePlayerProcessor::cannonLeft);
-		keyDownBindings.put(Keys.RIGHT, SinglePlayerProcessor::cannonRight);
-		keyDownBindings.put(Keys.SPACE, SinglePlayerProcessor::cannonShoot);
-
-		keyUpBindings.put(Keys.LEFT, SinglePlayerProcessor::cannonStopMoving);
-		keyUpBindings.put(Keys.RIGHT, SinglePlayerProcessor::cannonStopMoving);
-	}
-
-	@Override
-	public boolean keyDown(int keyCode) {
-		if (!keyDownBindings.containsKey(keyCode)) return false;
-		return keyDownBindings.get(keyCode).apply(this);
-	}
-
-	@Override
-	public boolean keyUp(int keyCode) {
-		if (!keyUpBindings.containsKey(keyCode)) return false;
-		return keyUpBindings.get(keyCode).apply(this);
+		super(BSMode);
 	}
 
 	public boolean cannonShoot() {
