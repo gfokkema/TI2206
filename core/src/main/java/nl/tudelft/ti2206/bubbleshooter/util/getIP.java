b96cedc3888c02bb30c40fec4fe8c03fadbc3bd5
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-09-21 20:19:25 +0200

    Documented getIP class

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/getIP.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/getIP.java
@@ -3,62 +3,32 @@ package nl.tudelft.ti2206.bubbleshooter.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
 
+/**
+ * This is a utility class that can be used to query the system for information
+ * about your local an external IP addresses.
+ */
 public class getIP {
-
+	/**
+	 * This method retrieves your local IP address using Java.
+	 * @return						a String with your local IP address
+	 * @throws UnknownHostException	when this method could not determine your IP
+	 */
 	public static String getLocalIP() throws UnknownHostException {
-		String ip = InetAddress.getLocalHost().getHostAddress().toString();
-		System.out.println("Local IP =" + ip);
-		return ip;
-
+		return InetAddress.getLocalHost().getHostAddress().toString();
 	}
 
-	public static int getPort() {
-		int port = 6969;
-		// testen of poort in gebruik is, moet nog ertussen!
-		return port;
-	}
-
-	// Werd me teveel try catch blokjes, daarom de I/O
+	/**
+	 * This method retrieves your external IP by using an amazon service.
+	 * @return				a String with your external IP address
+	 * @throws IOException	when this method could not determine your IP
+	 */
 	public static String getExternIP() throws IOException {
 		URL externIP = new URL("http://checkip.amazonaws.com");
-		BufferedReader read = null;
-		try {
-			read = new BufferedReader(new InputStreamReader(
-					externIP.openStream()));
-			String ip = read.readLine();
-			System.out.println("External IP = " + ip);
-			return ip;
-		} finally {
-			if (read != null) {
-				try {
-					read.close();
-				} catch (IOException e) {
-					System.out.println("mislukt");
-				}
-			}
-		}
+		BufferedReader read = new BufferedReader(new InputStreamReader(externIP.openStream()));
+		return read.readLine();
 	}
-
-	public static void main(String[] args) {
-		try {
-			getLocalIP();
-		} catch (UnknownHostException e) {
-
-			System.out.println("mislukt");
-		}
-		try {
-			getExternIP();
-		} catch (Exception e) {
-			System.out.println("mislukt");
-		}
-
-	}
-
 }
