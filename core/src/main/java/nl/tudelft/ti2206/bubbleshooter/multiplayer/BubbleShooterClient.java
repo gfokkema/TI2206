aa6828e8c4cc8ef79a390e4fe74ba953bcb6c497
Adam <hamier08@gmail.com>
2014-09-21 15:23:00 +0200

    clean up

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/multiplayer/BubbleShooterClient.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/multiplayer/BubbleShooterClient.java
@@ -1,5 +1,6 @@
 package nl.tudelft.ti2206.bubbleshooter.multiplayer;
 
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -8,35 +9,36 @@ import nl.tudelft.ti2206.bubbleshooter.core.Board;
 
 public class BubbleShooterClient extends Thread {
 
-	public static void main(String[] args) {
-		try {
+	public BubbleShooterClient() {
 
+	}
+
+	public void clientSocket() {
+
+		OutputStream s;
+		try {
 			Socket ClientSocket = new Socket(getIP.getLocalIP(),
 					getIP.getPort());
+			s = ClientSocket.getOutputStream();
 
-			// ipv getLocalIP, kan je ook getExternIP doen
-
-			OutputStream s = ClientSocket.getOutputStream();
 			ObjectOutputStream os = new ObjectOutputStream(s);
 			Board b = new Board(2, 6);
 
 			os.writeObject(b);
 
-			//
-			// PrintWriter output = new PrintWriter(
-			// ClientSocket.getOutputStream(), true);
-			// output.println("Connection succeeded");
-
-			// hier komt de precieze input voor klasse board die serialized word
-
 			os.flush();
 			s.close();
 			os.close();
 			ClientSocket.close();
+		} catch (IOException e) {
 
-		} catch (Exception iox) {
-			System.out.println("Connection horribly failed.");
+			e.printStackTrace();
 		}
+
 	}
 
+	public static void main(String[] args) {
+		BubbleShooterClient client = new BubbleShooterClient();
+		client.clientSocket();
+	}
 }
--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/multiplayer/BubbleShooterServer.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/multiplayer/BubbleShooterServer.java
@@ -2,67 +2,44 @@ package nl.tudelft.ti2206.bubbleshooter.multiplayer;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.io.PrintWriter;
-import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import nl.tudelft.ti2206.bubbleshooter.core.Board;
 
 public class BubbleShooterServer {
+	private ServerSocket gameListener = null;
+	private Socket socket = null;
+	private ObjectInputStream ois = null;
+	private InputStream is = null;
 
-	public static void main(String[] args) throws IOException {
+	public BubbleShooterServer() {
 
-		ServerSocket gameListener = new ServerSocket(getIP.getPort());
-		// ArrayList<Socket> socketList;
+	}
 
+	public void serverSocket() {
 		try {
-			// socketList = new ArrayList<Socket>();
+			gameListener = new ServerSocket(getIP.getPort());
+			socket = gameListener.accept();
 			while (true) {
-				// Socket socket = gameListener.accept();
-				// socketList.add(socket);
-				// try {
-				Socket socket1 = gameListener.accept();
-				InputStream is = socket1.getInputStream();
+				InputStream is = socket.getInputStream();
 				ObjectInputStream ois = new ObjectInputStream(is);
 				Board br = (Board) ois.readObject();
 
 				is.close();
 				ois.close();
-				socket1.close();
+				socket.close();
 				gameListener.close();
-				// } catch (Exception iox) {
-				// System.out
-				// .println("It isn't working, maybe later it will, maybe it will not. *For Windows-users: ERROR: PRESS OK");
-				// }
-
 			}
-		} catch (Exception iox) {
-			System.out.println("werkt niet");
+
+		} catch (Exception ex) {
+
 		}
 	}
+
+	public static void main(String[] args) throws IOException {
+		BubbleShooterServer server = new BubbleShooterServer();
+		server.serverSocket();
+	
+	}
 }
-
-// game BS = new launch();
-// BS.board board1 = BS.new Board(listener.accept(), ' key
-// voor ->');
-// board1(listener.accept(),'key voor <-');
-// board1(listener.accept().'key voor space');
-
-// BS.board board2 = BS.new Player(listener.accept(), ' key
-// voor ->');
-// board(listener.accept(),'key voor <-');
-// board(listener.accept().'key voor space');
-
-// BS.setOpponent(board1,board2);
-// board1.launch();
-// board2.launch();
-
-// OF en wss wat we gaan doen
-//
-// board als raw data doorheen sluizen
-// en steeds opnieuw drawen
-//
-//
