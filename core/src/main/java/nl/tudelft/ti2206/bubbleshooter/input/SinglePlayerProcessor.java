ba2d0031d9cd661cd8f650fde3d64d2e38ee24a6
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-09-21 22:04:49 +0200

    Added JavaDOC to the SinglePlayerProcessor

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/input/SinglePlayerProcessor.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/input/SinglePlayerProcessor.java
@@ -2,12 +2,24 @@ package nl.tudelft.ti2206.bubbleshooter.input;
 
 import nl.tudelft.ti2206.bubbleshooter.mode.BSMode;
 
+/**
+ * This class processes input from a single player game.
+ * It will listen for input from the controls and call actions based on that. 
+ *
+ */
 public class SinglePlayerProcessor extends AbstractProcessor {
-
+	/**
+	 * Create a {@link SinglePlayerProcessor} which will control the given {@link BSMode}.
+	 * @param BSMode	the {@link BSMode} to control
+	 */
 	public SinglePlayerProcessor(BSMode BSMode) {
 		super(BSMode);
 	}
 
+	/**
+	 * This function is called to shoot the cannon in the {@link BSMode} game.
+	 * @return	boolean indicating whether the cannon could be fired
+	 */
 	public boolean cannonShoot() {
 		if (mode.getProjectile() == null) {
 			mode.setProjectile(mode.getCannon().shoot());
@@ -16,21 +28,37 @@ public class SinglePlayerProcessor extends AbstractProcessor {
 		return false;
 	}
 
+	/**
+	 * This function will start rotating the cannon in the {@link BSMode} to the left.
+	 * @return	boolean indicating whether the cannon has started rotating
+	 */
 	public boolean cannonLeft() {
 		mode.cannonLeft(true);
 		return true;
 	}
 
+	/**
+	 * This function will start rotating the cannon in the {@link BSMode} to the right.
+	 * @return	boolean indicating whether the cannon has started rotating
+	 */
 	public boolean cannonRight() {
 		mode.cannonRight(true);
 		return true;
 	}
 
+	/**
+	 * This function will stop rotating the cannon in the {@link BSMode} to the left.
+	 * @return	boolean indicating whether the cannon has stopped rotating
+	 */
 	public boolean cannonStopLeft() {
 		mode.cannonLeft(false);
 		return true;
 	}
 
+	/**
+	 * This function will stop rotating the cannon in the {@link BSMode} to the right.
+	 * @return	boolean indicating whether the cannon has stopped rotating
+	 */
 	public boolean cannonStopRight() {
 		mode.cannonRight(false);
 		return true;
