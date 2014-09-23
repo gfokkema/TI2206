607863e6d583505bb8781f67a722b78a6891f671
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-09-23 10:23:31 +0200

    Deleted the incorrect report

--- core/src/test/java/nl/tudelft/ti2206/bubbleshooter/mode/SinglePlayerModeTest.java
+++ core/src/test/java/nl/tudelft/ti2206/bubbleshooter/mode/SinglePlayerModeTest.java
@@ -1,7 +1,6 @@
 package nl.tudelft.ti2206.bubbleshooter.mode;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyFloat;
 import static org.mockito.Matchers.anyInt;
