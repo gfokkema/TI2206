e429a2a447c689d1140478b7e56183d3caf548ce
Adam <hamier08@gmail.com>
2014-09-27 13:00:41 +0200

    externe testje gefixt

--- core/src/test/java/nl/tudelft/ti2206/bubbleshooter/util/getIPTest.java
+++ core/src/test/java/nl/tudelft/ti2206/bubbleshooter/util/getIPTest.java
@@ -1,63 +1,51 @@
 package nl.tudelft.ti2206.bubbleshooter.util;
 
-
- 		 
 import static org.junit.Assert.assertEquals;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.URL;
+import org.junit.Test;
 
-import org.mockito.*;		
+public class getIPTest {
 
-import java.io.BufferedReader;		
-import java.io.InputStreamReader;		 
-import java.net.Inet4Address;		 
-import java.net.InetAddress;		
-import java.net.URL;		 
-import java.net.UnknownHostException;		
- 		 
+	@Test
+	public void getExternIPTest() throws Exception {
+		URL externIP = new URL("http://icanhazip.com");
+		BufferedReader read = new BufferedReader(new InputStreamReader(
+				externIP.openStream()));
+		String externIp = read.readLine();
 
- import org.junit.Test;		 
- 		 
+		assertEquals(externIp, getIP.getExternIP());
+	}
 
-import nl.tudelft.ti2206.bubbleshooter.core.Bubble;		
-	
+	@Test
+	public void getLocalIPTest() throws Exception {
+		String Local = Inet4Address.getLocalHost().getHostAddress();
 
-import com.badlogic.gdx.graphics.Color;		
-		
- public class getIPTest {		 
- 		
-	 @Test
-	 public void getExternIPTest() throws Exception {
-	 		URL externIP = new URL("http://icanhazip.com");
-	 		BufferedReader read = new BufferedReader(new InputStreamReader(
-	 				externIP.openStream()));
-	 }
-	 
-	 @Test
-	 public void getLocalIPTest() throws Exception {
-	 		String Local = Inet4Address.getLocalHost().getHostAddress().toString();
-	 		
-	 		assertEquals(Local, getIP.getLocalIP());
-	 	}
-	 
-	 /** 
-	  * We zijn uitgegaan van de conventionele localhost, 
-	  * en houden dus geen rekening met zelf gewijzigde systemen.
-	  * @throws Exception
-	  */
-	 @Test
-	 public void getLoopbackIPTest() throws Exception {
-	 		String loopback = "localhost/127.0.0.1";
-	 		assertEquals(loopback,getIP.getLoopbackIP());
-	 	} 
-	 
-	 
-	 
-	//TEST linux, blijkbaar returned ie op windows ipv 4, maar op linux wel ipv6 als je kan testen en
-	//	anders deleten
-	//@Test
-	//	public void getLocalIPnot6Test() throws Exception{
-	//		String Local = Inet6Address.getLocalHost().
-	//		System.out.println(Local);
-	//		assertEquals(Local, getIP.getLocalIP());
-	//	}
-	 
- }
+		assertEquals(Local, getIP.getLocalIP());
+	}
+
+	/**
+	 * We zijn uitgegaan van de conventionele localhost, en houden dus geen
+	 * rekening met zelf gewijzigde systemen.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void getLoopbackIPTest() throws Exception {
+		String loopback = "localhost/127.0.0.1";
+		assertEquals(loopback, getIP.getLoopbackIP());
+	}
+
+	// TEST linux, blijkbaar returned ie op windows ipv 4, maar op linux wel
+	// ipv6 als je kan testen en
+	// anders deleten
+	// @Test
+	// public void getLocalIPnot6Test() throws Exception{
+	// String Local = Inet6Address.getLocalHost().getHostAddress();
+	// System.out.println(Local);
+	// assertEquals(Local, getIP.getLocalIP());
+	// }
+
+}
