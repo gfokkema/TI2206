93445778977925deef09c7d58954e51020f5face
Owen <0w3nhuang@gmail.com>
2014-10-11 00:38:07 +0200

    Add basic tests for the various special bubbles

---
+++ core/src/test/java/nl/tudelft/ti2206/bubbleshooter/core/BomBehaviourTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.ti2206.bubbleshooter.core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BomBehaviourTest {
+
+	@Test
+	public void test() {
+		//TODO mock testing...
+	}
+
+}
---
+++ core/src/test/java/nl/tudelft/ti2206/bubbleshooter/core/StoneBubbleBehaviourTest.java
@@ -0,0 +1,13 @@
+package nl.tudelft.ti2206.bubbleshooter.core;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class StoneBubbleBehaviourTest {
+
+	@Test
+	public void test() {
+		//TODO mock testing...
+	}
+
+}
