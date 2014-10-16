07f95a83827405d9cedb0195ebcfc63aa318e957
Adam <hamier08@gmail.com>
2014-10-16 22:26:42 +0200

    javadoc for highscore

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/HighScore.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/HighScore.java
@@ -1,22 +1,39 @@
 package nl.tudelft.ti2206.bubbleshooter.util;
-
+/**
+ * A class which creates a object for the HighScoresFile.
+ * @author group-15
+ *
+ */
 public class HighScore implements Comparable<HighScore> {
 	private int highscore;
 	private String name;
 	
+	/**
+	 * A method to create a HighScore object.
+	 * @param name  The name given by the player
+	 * @param score The score of the player
+	 */
 	public HighScore(String name, int highscore) {
 		this.highscore = highscore;
 		this.name = name;
 	}
-
+	/**
+	 * 
+	 * @returns the score
+	 */
 	public int getHighScore() {
 		return highscore;
 	}
-
+	/**
+	 * 
+	 * @returns the name
+	 */
 	public String getName() {
 		return name;
 	}
-
+	/**
+	 * A method to help to SortedSet to become sorted.
+	 */
 	@Override
 	public int compareTo(HighScore o) {
 		return Integer.compare(this.highscore, o.highscore);
