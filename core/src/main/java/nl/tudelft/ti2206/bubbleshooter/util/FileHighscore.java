af799c411b9dbdb98ba1d853056bc7ede32081b5
Adam <hamier08@gmail.com>
2014-10-16 22:09:40 +0200

    fileHighScore implemented

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/FileHighscore.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/FileHighscore.java
@@ -1,23 +1,62 @@
 package nl.tudelft.ti2206.bubbleshooter.util;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.SortedSet;
 
 public class FileHighscore {
 	private SortedSet<HighScore> scores;
 
-	public void addScore(String naam, int score){
+	public void addScore(String naam, int score) {
 		loadScoreFile();
-		scores.add(new HighScore(naam,score));
+		scores.add(new HighScore(naam, score));
 		updateScoreFile();
 	}
-	
-	private void updateScoreFile() {
-		// TODO Auto-generated method stub
-		
+
+	@SuppressWarnings("unchecked")
+	public void loadScoreFile() {
+		ObjectInputStream inputStream = null;
+		try {
+			inputStream = new ObjectInputStream(new FileInputStream(
+					"HighScores.txt"));
+			scores = (SortedSet<HighScore>) inputStream.readObject();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
 	}
 
-	public void loadScoreFile() {
-		// TODO Auto-generated method stub
-		
+	private void updateScoreFile() {
+		ObjectOutputStream outputStream = null;
+		try {
+			outputStream = new ObjectOutputStream(new FileOutputStream(
+					"HighScores.txt"));
+			outputStream.writeObject(scores);
+
+		} catch (Exception e) {
+			System.out.println("erorroeorero");
+		} finally {
+			try {
+				if (outputStream != null) {
+					outputStream.flush();
+					outputStream.close();
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
 	}
 }
