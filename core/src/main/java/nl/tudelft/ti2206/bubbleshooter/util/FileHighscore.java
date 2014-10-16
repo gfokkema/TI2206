849deb514290a76ff0b3c5a2d0892d36ec67f687
Adam <hamier08@gmail.com>
2014-10-16 22:22:39 +0200

    javadoc added to FileHighscore

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/FileHighscore.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/FileHighscore.java
@@ -6,15 +6,28 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.SortedSet;
 
+/**
+ * A class to read or write to the highscores file.
+ * @author group-15
+ *
+ */
 public class FileHighscore {
 	private SortedSet<HighScore> scores;
 
-	public void addScore(String naam, int score) {
+	/**
+	 * A method to add a HighScore object
+	 * @param name  The name given by the player
+	 * @param score The score of the player
+	 */
+	public void addScore(String name, int score) {
 		loadScoreFile();
-		scores.add(new HighScore(naam, score));
+		scores.add(new HighScore(name, score));
 		updateScoreFile();
 	}
 
+	/**
+	 * A method to load the highscore file in, which closes the inputreader after the file is read.
+	 */
 	@SuppressWarnings("unchecked")
 	public void loadScoreFile() {
 		ObjectInputStream inputStream = null;
@@ -37,7 +50,9 @@ public class FileHighscore {
 		}
 
 	}
-
+	/**
+	 *  A method which writes back to the highscores file, and closes the outputreader after.
+	 */
 	private void updateScoreFile() {
 		ObjectOutputStream outputStream = null;
 		try {
