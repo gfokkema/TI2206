da71710d3ed29303ed05afeee32657bcc445c407
Adam <hamier08@gmail.com>
2014-10-18 16:02:17 +0200

    lege file method

---
+++ core/src/test/java/nl/tudelft/ti2206/bubbleshooter/util/FileHighscoreTest.java
@@ -0,0 +1,44 @@
+package nl.tudelft.ti2206.bubbleshooter.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileHighscoreTest {
+
+	@Test
+	public void testFileHighscore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFileName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFileName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadScoreFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsHighScore() {
+		fail("Not yet implemented");
+	}
+
+}
