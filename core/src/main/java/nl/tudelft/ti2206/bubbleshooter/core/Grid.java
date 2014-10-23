06eac4c210db1e94523741c3fb3dd1e1f0d4b1ef
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-10-24 00:34:20 +0200

    Fixed the synchronisation issue and with it the multiplayer EndingCondition

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/core/Grid.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/core/Grid.java
@@ -63,7 +63,7 @@ public class Grid extends BSDrawable implements Serializable, Collidable, Observ
 		cell.setBubble(b);
 
 		setChanged();
-		notifyObservers("Bubble has been added to index " + idx + ".");
+		notifyObservers("Grid: Bubble has been added to index " + idx + ".");
 		return cell;
 	}
 
@@ -149,6 +149,8 @@ public class Grid extends BSDrawable implements Serializable, Collidable, Observ
 		Collection<GridCell> disconnected = new HashSet<GridCell>(getFilledGridCells());
 		disconnected.removeAll(connected);
 		disconnected.forEach((GridCell gc) -> gc.removeBubble());
+		setChanged();
+		notifyObservers("Grid: Removing " + disconnected.size() + "disconnected bubbles.");
 		return disconnected.size();
 	}
 
