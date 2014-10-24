8bb3b4032b1011645ad9ae3f4260ff45a97eeb2d
adamIqbal <hamier08@gmail.com>
2014-10-25 01:45:49 +0200

    halverwege chatbox

---
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/util/chat/chat.java
@@ -0,0 +1,12 @@
+package nl.tudelft.ti2206.bubbleshooter.util.chat;
+
+import java.io.Serializable;
+
+public class chat implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5106943752477003211L;
+	ChatMessage[] ChatMessage = new ChatMessage[10];
+}
