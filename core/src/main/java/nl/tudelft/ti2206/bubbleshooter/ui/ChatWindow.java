51db56a944eb9400c4a79543d346157c1099228d
Balletie <skip_meesie@hotmail.com>
2014-10-25 20:06:49 +0200

    Fix errors in ChatWindow

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/ui/ChatWindow.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/ui/ChatWindow.java
@@ -1,13 +1,14 @@
 package nl.tudelft.ti2206.bubbleshooter.ui;
 
 import nl.tudelft.ti2206.bubbleshooter.util.ChatObserver;
+import nl.tudelft.ti2206.bubbleshooter.util.chat.ChatMessage;
 
 public class ChatWindow implements ChatObserver {
 	ChatMessage msg = new ChatMessage("");
 
 	@Override
-	public void addMessage(String msg) {
-		msg = new ChatMessage(msg);
+	public void addMessage(String msgstr) {
+		this.msg = new ChatMessage(msgstr);
 
 	}
 
