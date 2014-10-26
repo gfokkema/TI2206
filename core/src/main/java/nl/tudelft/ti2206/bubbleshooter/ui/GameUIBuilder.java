6b531a5cac3775acf4293e07878b619e5dbfe654
Balletie <skip_meesie@hotmail.com>
2014-10-26 23:54:08 +0100

    Also update the opponent statsbar at least once. Add JavaDoc for GameUIBuilder

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/ui/GameUIBuilder.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/ui/GameUIBuilder.java
@@ -2,6 +2,7 @@ package nl.tudelft.ti2206.bubbleshooter.ui;
 
 import nl.tudelft.ti2206.bubbleshooter.mode.conditions.EndingCondition;
 import nl.tudelft.ti2206.bubbleshooter.score.Score;
+import nl.tudelft.ti2206.bubbleshooter.util.StatsObserver;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -22,6 +23,12 @@ public class GameUIBuilder {
 		table.setFillParent(true);
 	}
 
+	/**
+	 * Add a {@link StatsBar}, and add it as {@link StatsObserver} to the 
+	 * {@link EndingCondition} and {@link Score}.
+	 * @param end	The {@link EndingCondition} for adding the {@link StatsObserver}
+	 * @param score	The {@link Score} for adding the {@link StatsObserver}
+	 */
 	public void addSinglePlayerStatsBar(EndingCondition end, Score score) {
 		StatsBar single = addStatsBar(true);
 		end.addStatsObserver(single);
@@ -29,12 +36,25 @@ public class GameUIBuilder {
 		single.updateScore(score);
 	}
 
+	/**
+	 * Add two {@link StatsBar}s, and add them as {@link StatsObserver} to the 
+	 * {@link EndingCondition} and {@link Score} and the opponent's {@link Score}.
+	 * @param end		The {@link EndingCondition} for adding the {@link StatsObserver}
+	 * @param score		The {@link Score} for adding the {@link StatsObserver}
+	 * @param oppscore	The opponent's {@link Score} for adding the {@link StatsObserver}
+	 */
 	public void addMultiPlayerStatsBars(EndingCondition end, Score score, Score oppscore) {
 		addSinglePlayerStatsBar(end, score);
 		StatsBar multi = addStatsBar(false);
 		oppscore.addStatsObserver(multi);
+		multi.updateScore(oppscore);
 	}
 
+	/**
+	 * Create a {@link StatsBar}, and add its {@link Label}s to the {@link Table}.
+	 * @param left true or false, if you want it left or right respectively.
+	 * @return	The created {@link StatsBar}.
+	 */
 	public StatsBar addStatsBar(boolean left) {
 		VerticalGroup stats = new VerticalGroup();
 		stats.align(Align.left);
@@ -48,11 +68,19 @@ public class GameUIBuilder {
 		return new StatsBar(timer, score);
 	}
 
+	/**
+	 * Utility method to instantiate a {@link Label}
+	 * @return	the {@link Label}
+	 */
 	private Label getLabel() {
 		LabelStyle style = new LabelStyle(font, Color.WHITE);
 		return new Label(null, style);
 	}
 
+	/**
+	 * Build the {@link GameUI}.
+	 * @return	the {@link GameUI} as a result of the build process.
+	 */
 	public GameUI build() {
 		return new GameUI(table);
 	}
