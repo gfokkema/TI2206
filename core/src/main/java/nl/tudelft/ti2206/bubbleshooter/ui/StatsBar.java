17bd76eae3880683b8dc2ec49c0d935953a3cfb4
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-10-26 23:15:55 +0100

    Added a label for the level name to the game ui

--- core/src/main/java/nl/tudelft/ti2206/bubbleshooter/ui/StatsBar.java
+++ core/src/main/java/nl/tudelft/ti2206/bubbleshooter/ui/StatsBar.java
@@ -10,11 +10,13 @@ import nl.tudelft.ti2206.bubbleshooter.util.StatsObserver;
 public class StatsBar implements StatsObserver {
 	protected Label timerField;
 	protected Label scoreField;
+	protected Label levelName;
 	protected static String format = "Time left: %02d:%02d";
 
-	protected StatsBar(Label timer, Label score) {
+	protected StatsBar(Label timer, Label score, Label level) {
 		this.timerField = timer;
 		this.scoreField = score;
+		this.levelName = level;
 	}
 
 	@Override
@@ -26,6 +28,7 @@ public class StatsBar implements StatsObserver {
 	@Override
 	public void updateScore(Score score) {
 		scoreField.setText("Score:" + score.getScore());
+		levelName.setText("Level " + score.getLevel().getLevel() + ": " + score.getLevel().getName());
 	}
 	
 }
